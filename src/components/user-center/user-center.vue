ae8184cbd6b774bde2a6781194f285f51a697ca0
weite122 <949193531@qq.com>
2018-02-09 00:50:11 +0800

    [A]user-center组件添加播放列表和收藏列表

--- src/components/user-center/user-center.vue
+++ src/components/user-center/user-center.vue
@@ -12,13 +12,27 @@
         <span class="text">随机播放全部</span>
       </div>
       <div class="list-wrapper" ref="listWrapper">
-       </div>
+        <scroll ref="favoriteList" class="list-scroll" v-if="currentIndex===0" :data="favoriteList">
+          <div class="list-inner">
+            <song-list :songs="favoriteList" @select="selectSong"></song-list>
+          </div>
+        </scroll>
+        <scroll ref="playList" class="list-scroll" v-if="currentIndex===1" :data="playHistory">
+          <div class="list-inner">
+            <song-list :songs="playHistory" @select="selectSong"></song-list>
+          </div>
+        </scroll>
+      </div>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import Switches from 'base/switches/switches'
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+  import Song from 'common/js/song'
+  import {mapGetters, mapActions} from 'vuex'
 
   export default {
     data() {
@@ -31,15 +45,26 @@
       }
     },
     computed: {
-
+      ...mapGetters([
+        'favoriteList',
+        'playHistory'
+      ])
     },
     methods: {
       switchItem(index) {
         this.currentIndex = index
-      }
+      },
+      selectSong(song) {
+        this.insertSong(new Song(song))
+      },
+      ...mapActions([
+        'insertSong'
+      ])
     },
     components: {
-      Switches
+      Switches,
+      Scroll,
+      SongList
     }
   }
 </script>
